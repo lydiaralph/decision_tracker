f7421544a670e4aec0aa288359e688915f8a75ed
Lydia Ralph <lydia.ralph@bbc.co.uk>
2019-01-18 23:02:44 +0000

    Fixed imports and dependencies. WIP: database classes

--- app/src/main/java/com/lydiaralph/decisiontracker/MainActivity.java
+++ app/src/main/java/com/lydiaralph/decisiontracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lydiaralph.decisiontracker;
 
+import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
