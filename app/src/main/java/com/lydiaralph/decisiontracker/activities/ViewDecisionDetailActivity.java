013de3d9c57eb694b63297fc10a95c23d6814eac
Lydia Ralph <lydia.ralph@bbc.co.uk>
2019-06-05 15:37:56 +0100

    WIP barchart

--- app/src/main/java/com/lydiaralph/decisiontracker/activities/ViewDecisionDetailActivity.java
+++ app/src/main/java/com/lydiaralph/decisiontracker/activities/ViewDecisionDetailActivity.java
@@ -2,17 +2,34 @@ package com.lydiaralph.decisiontracker.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.model.GradientColor;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
 import com.lydiaralph.decisiontracker.R;
+import com.lydiaralph.decisiontracker.charts.DayAxisValueFormatter;
+import com.lydiaralph.decisiontracker.charts.MyValueFormatter;
 import com.lydiaralph.decisiontracker.charts.PieChartDisplay;
+import com.lydiaralph.decisiontracker.charts.XYMarkerView;
 import com.lydiaralph.decisiontracker.database.entity.DecisionOptions;
 import com.lydiaralph.decisiontracker.database.entity.MoodDescriptionWithIntensity;
 import com.lydiaralph.decisiontracker.database.entity.OptionsVotes;
@@ -25,11 +42,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -39,7 +58,8 @@ import dagger.android.AndroidInjection;
  * Inspired by https://github.com/PhilJay/MPAndroidChart/blob/master/MPChartExample/src/main/java/com/xxmassdeveloper/mpchartexample/PieChartActivity.java
  */
 
-public class ViewDecisionDetailActivity extends MenuBasedActivity implements OnChartValueSelectedListener, TerminateDecisionTrackingFragment.Listener {
+public class ViewDecisionDetailActivity extends MenuBasedActivity implements OnChartValueSelectedListener, TerminateDecisionTrackingFragment.Listener,
+        SeekBar.OnSeekBarChangeListener {
     private static final String LOG = ViewDecisionDetailActivity.class.getSimpleName();
 
     @Inject
@@ -58,7 +78,7 @@ public class ViewDecisionDetailActivity extends MenuBasedActivity implements OnC
 
         Integer selectedDecisionId = (Integer) getIntent().getExtras().get(ViewDecisionsCategoryActivity.VIEW_DECISION_ID);
         decisionViewModel = ViewModelProviders.of(this, viewModelFactory).get(DecisionViewModel.class);
-        moodViewModel = ViewModelProviders.of(this, viewModelFactory).get(MoodViewModel.class);
+        moodViewModel = ViewModelProviders.of(this, viewModelFactory).get(MoodVif vewModel.class);
 
         final Observer<DecisionOptions> decisionObserver = getDecisionOptionsObserver();
         final Observer<List<MoodDescriptionWithIntensity>> moodObserver = getMoodObserver();
@@ -119,11 +139,10 @@ public class ViewDecisionDetailActivity extends MenuBasedActivity implements OnC
                         if (!hasVotes) {
                             editorialTextView.setText(getString(R.string.no_votes_for_this_decision));
                         } else {
-                            PieChart chart = findViewById(R.id.chart1);
-                            PieChartDisplay pieChartDisplay = new PieChartDisplay(chart, decision);
-                            pieChartDisplay.displayVotesInPieChart();
-                            pieChartDisplay.getChart().setOnChartValueSelectedListener(ViewDecisionDetailActivity.this);
-                            //                        editorialTextView.setText(R.string.you_decided);
+                            setPieChart(decision);
+                            BarChart barChart = new BarChart(ViewDecisionDetailActivity.this);
+                            List<BarEntry> entries = new ArrayList<BarEntry>();
+                            makeABarChart();
                         }
 
 //                        for (OptionsVotes option : decision.getOptionsList()) {
@@ -172,6 +191,50 @@ public class ViewDecisionDetailActivity extends MenuBasedActivity implements OnC
         };
     }
 
+    private void makeABarChart() {
+        BarChart chart = findViewById(R.id.barchart);
+
+        ArrayList NoOfEmp = new ArrayList();
+
+        NoOfEmp.add(new BarEntry(945f, 0));
+        NoOfEmp.add(new BarEntry(1040f, 1));
+        NoOfEmp.add(new BarEntry(1133f, 2));
+        NoOfEmp.add(new BarEntry(1240f, 3));
+        NoOfEmp.add(new BarEntry(1369f, 4));
+        NoOfEmp.add(new BarEntry(1487f, 5));
+        NoOfEmp.add(new BarEntry(1501f, 6));
+        NoOfEmp.add(new BarEntry(1645f, 7));
+        NoOfEmp.add(new BarEntry(1578f, 8));
+        NoOfEmp.add(new BarEntry(1695f, 9));
+
+        ArrayList year = new ArrayList();
+
+        year.add("2008");
+        year.add("2009");
+        year.add("2010");
+        year.add("2011");
+        year.add("2012");
+        year.add("2013");
+        year.add("2014");
+        year.add("2015");
+        year.add("2016");
+        year.add("2017");
+
+        BarDataSet bardataset = new BarDataSet(NoOfEmp, "No Of Employee");
+        chart.animateY(5000);
+
+        BarData data = new BarData(bardataset);
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        chart.setData(data);
+    }
+
+    private void setPieChart(@NotNull DecisionOptions decision) {
+        PieChart chart = findViewById(R.id.chart1);
+        PieChartDisplay pieChartDisplay = new PieChartDisplay(chart, decision);
+        pieChartDisplay.displayVotesInPieChart();
+        pieChartDisplay.getChart().setOnChartValueSelectedListener(ViewDecisionDetailActivity.this);
+    }
+
     private String getFormattedDateString(@NotNull DecisionOptions decision) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
         return String.format("%s - %s",
@@ -192,6 +255,177 @@ public class ViewDecisionDetailActivity extends MenuBasedActivity implements OnC
     public void onNothingSelected() {
         Log.i("PieChart", "nothing selected");
     }
+
+
+    private BarChart chart;
+
+    private SeekBar seekBarX, seekBarY;
+    private TextView tvX, tvY;
+
+    public void displayVotesInBarChart(){
+        // TODO
+    }
+
+    private void configureChart() {
+        chart.setDrawBarShadow(false);
+        chart.setDrawValueAboveBar(true);
+
+        chart.getDescription().setEnabled(false);
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        chart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        chart.setPinchZoom(false);
+
+        chart.setDrawGridBackground(false);
+        // chart.setDrawYLabels(false);
+
+        ValueFormatter xAxisFormatter = new DayAxisValueFormatter(chart);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1f); // only intervals of 1 day
+        xAxis.setLabelCount(7);
+        xAxis.setValueFormatter(xAxisFormatter);
+
+        ValueFormatter custom = new MyValueFormatter("$");
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setLabelCount(8, false);
+        leftAxis.setValueFormatter(custom);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setLabelCount(8, false);
+        rightAxis.setValueFormatter(custom);
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setForm(Legend.LegendForm.SQUARE);
+        l.setFormSize(9f);
+        l.setTextSize(11f);
+        l.setXEntrySpace(4f);
+
+        XYMarkerView mv = new XYMarkerView(this, xAxisFormatter);
+        mv.setChartView(chart); // For bounds control
+        chart.setMarker(mv); // Set the marker to the chart
+
+        // setting data
+        seekBarY.setProgress(50);
+        seekBarX.setProgress(12);
+
+    }
+
+    private void setData(int count, float range) {
+
+        float start = 1f;
+
+        ArrayList<BarEntry> values = new ArrayList<>();
+
+        for (int i = (int) start; i < start + count; i++) {
+            float val = (float) (Math.random() * (range + 1));
+
+//            if (Math.random() * 100 < 25) {
+//                values.add(new BarEntry(i, val, getResources().getDrawable(R.drawable.star)));
+//            } else {
+                values.add(new BarEntry(i, val));
+//            }
+        }
+
+        BarDataSet set1;
+
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+
+        } else {
+            set1 = new BarDataSet(values, "The year 2017");
+
+            set1.setDrawIcons(false);
+
+//            set1.setColors(ColorTemplate.MATERIAL_COLORS);
+
+            /*int startColor = ContextCompat.getColor(this, android.R.color.holo_blue_dark);
+            int endColor = ContextCompat.getColor(this, android.R.color.holo_blue_bright);
+            set1.setGradientColor(startColor, endColor);*/
+
+            int startColor1 = ContextCompat.getColor(this, android.R.color.holo_orange_light);
+            int startColor2 = ContextCompat.getColor(this, android.R.color.holo_blue_light);
+            int startColor3 = ContextCompat.getColor(this, android.R.color.holo_orange_light);
+            int startColor4 = ContextCompat.getColor(this, android.R.color.holo_green_light);
+            int startColor5 = ContextCompat.getColor(this, android.R.color.holo_red_light);
+            int endColor1 = ContextCompat.getColor(this, android.R.color.holo_blue_dark);
+            int endColor2 = ContextCompat.getColor(this, android.R.color.holo_purple);
+            int endColor3 = ContextCompat.getColor(this, android.R.color.holo_green_dark);
+            int endColor4 = ContextCompat.getColor(this, android.R.color.holo_red_dark);
+            int endColor5 = ContextCompat.getColor(this, android.R.color.holo_orange_dark);
+
+            List<GradientColor> gradientColors = new ArrayList<>();
+            gradientColors.add(new GradientColor(startColor1, endColor1));
+            gradientColors.add(new GradientColor(startColor2, endColor2));
+            gradientColors.add(new GradientColor(startColor3, endColor3));
+            gradientColors.add(new GradientColor(startColor4, endColor4));
+            gradientColors.add(new GradientColor(startColor5, endColor5));
+
+            set1.setGradientColors(gradientColors);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(0.9f);
+
+            chart.setData(data);
+        }
+    }
+
+    private List<Integer> getColors(){
+        List<Integer> colors = new ArrayList<>();
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+
+        return colors;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        tvX.setText(String.valueOf(seekBarX.getProgress()));
+        tvY.setText(String.valueOf(seekBarY.getProgress()));
+
+        setData(seekBarX.getProgress(), seekBarY.getProgress());
+        chart.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+    private final RectF onValueSelectedRectF = new RectF();
+
 
 }
 
