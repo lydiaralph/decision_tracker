28b68a6640a3a7d1e0f6848ef842fc7e450a54e8
Lydia Ralph <lydia.ralph@bbc.co.uk>
2019-06-12 14:07:41 +0100

    WIP: added spinner to choose fragment on display

--- app/src/main/java/com/lydiaralph/decisiontracker/activities/ChartDisplayActivity.java
+++ app/src/main/java/com/lydiaralph/decisiontracker/activities/ChartDisplayActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -11,11 +12,17 @@ import com.lydiaralph.decisiontracker.R;
 import com.lydiaralph.decisiontracker.fragments.ArticleFragment;
 import com.lydiaralph.decisiontracker.fragments.HeadlinesFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import androidx.fragment.app.FragmentActivity;
 
 public class ChartDisplayActivity extends FragmentActivity {
     private static final String LOG_NAME = ChartDisplayActivity.class.getSimpleName();
-//    private Spinner chooseChartOptionSpinner;
+    private Spinner chooseChartOptionSpinner;
+
+    private HeadlinesFragment headlinesFragment;
+    private ArticleFragment articleFragment;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,55 +30,64 @@ public class ChartDisplayActivity extends FragmentActivity {
         Log.i(LOG_NAME, "Starting activity");
         setContentView(R.layout.chart_display);
 
-
-        // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
         if (findViewById(R.id.fragment_container) != null) {
             Log.i(LOG_NAME, "Creating fragments");
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
             if (savedInstanceState != null) {
                 return;
             }
 
-            // Create a new Fragment to be placed in the activity layout
-            HeadlinesFragment firstFragment = new HeadlinesFragment();
-            ArticleFragment secondFragment = new ArticleFragment();
+            headlinesFragment = new HeadlinesFragment();
+            articleFragment = new ArticleFragment();
 
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-            firstFragment.setArguments(getIntent().getExtras());
-            secondFragment.setArguments(getIntent().getExtras());
+            headlinesFragment.setArguments(getIntent().getExtras());
+            articleFragment.setArguments(getIntent().getExtras());
 
-            // Add the fragment to the 'fragment_container' FrameLayout
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, firstFragment)
-//                    .add(R.id.fragment_container, secondFragment)
+                    .add(R.id.fragment_container, articleFragment)
                     .commit();
 
-//            chooseChartOptionSpinner = findViewById(R.id.spinner1);
+            addItemsOnSpinner();
 
         }
     }
 
-//    @Override
-//    public void onStart(){
-//        super.onStart();
-//        chooseChartOptionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View selectedItemView, int position, long id) {
-//                Toast.makeText(parent.getContext(),
-//                        "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parentView) {
-//                // Do nothing
-//            }
-//
-//        });
-//    }
+    public void addItemsOnSpinner() {
+        chooseChartOptionSpinner = findViewById(R.id.spinner1);
+        List<String> list = new ArrayList<>();
+        list.add("Articles");
+        list.add("Headlines");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        chooseChartOptionSpinner.setAdapter(dataAdapter);
+
+        chooseChartOptionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View selectedItemView, int position, long id) {
+
+                String chosenItem = parent.getItemAtPosition(position).toString();
+
+                if(chosenItem.equals("Headlines")) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, headlinesFragment)
+                            .commit();
+                } else if (chosenItem.equals("Articles")){
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, articleFragment)
+                            .commit();
+                }
+
+                Toast.makeText(parent.getContext(),
+                        "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Do nothing
+            }
+
+        });
+    }
 }
